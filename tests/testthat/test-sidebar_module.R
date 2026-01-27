4cb20d2c25d7a11873e7fc51fc057c8514177e6c
Luís Gustavo Schuck <luisgustavoschuck@yahoo.com.br>
2026-01-27 10:34:50 -0300

    New test for change active dataset in sidebar module test

--- tests/testthat/test-sidebar_module.R
+++ tests/testthat/test-sidebar_module.R
@@ -1,7 +1,104 @@
 # tests/testthat/test-sidebar_module.R
 
+# test change active dataset --------------------------------------------------
+test_that('Test change active dataset', {
+  testServer(sidebar_server, args = list(app_session = NULL), {
+
+    session$userData$dt <- reactiveValues(
+      dt = list('df_iris' = iris |> as.data.table(),
+                'df_mtcars' = mtcars |> as.data.table()),
+      act_name = 'df_iris'
+    )
+
+    session$userData$dt_names <- reactive({
+      req(session$userData$dt$dt)
+      names(session$userData$dt$dt)
+    })
+
+    session$setInputs(sel_datasets_names = 'iris')
+
+    session$userData$dt_names <- reactive({
+      req(session$userData$dt$dt)
+      names(session$userData$dt$dt)
+    })
+
+    session$setInputs(sel_act_dt = 'df_mtcars')
+
+    expect_true(session$userData$dt$act_name == 'df_mtcars')
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 mtcars |> as.data.table())
+
+    session$setInputs(sel_act_dt = 'df_iris')
+
+    expect_true(session$userData$dt$act_name == 'df_iris')
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 iris |> as.data.table())
+
+    session$setInputs(sel_act_dt = 'df_mtcars')
+
+    expect_true(session$userData$dt$act_name == 'df_mtcars')
+    expect_equal(session$userData$dt$dt |> names(), c('df_iris', 'df_mtcars'))
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 mtcars |> as.data.table()
+    )
+  })
+})
+
+test_that('Test active dataset - after deletion', {
+  testServer(sidebar_server, args = list(app_session = NULL), {
+
+
+    session$userData$dt <- reactiveValues(
+      dt = list('df_iris' = iris |> as.data.table(),
+                'df_mtcars' = mtcars |> as.data.table(),
+                'df_cars' = cars |> as.data.table()
+                ),
+      act_name = 'df_iris'
+    )
+
+    session$userData$dt_names <- reactive({
+      req(session$userData$dt$dt)
+      names(session$userData$dt$dt)
+    })
+
+    session$setInputs(sel_datasets_names = 'iris')
+
+    session$userData$dt_names <- reactive({
+      req(session$userData$dt$dt)
+      names(session$userData$dt$dt)
+    })
+
+    session$setInputs(sel_act_dt = 'df_mtcars')
+
+    expect_true(session$userData$dt$act_name == 'df_mtcars')
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 mtcars |> as.data.table())
+
+    session$setInputs(sel_act_dt = 'df_iris')
+
+    expect_true(session$userData$dt$act_name == 'df_iris')
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 iris |> as.data.table())
+
+    session$setInputs(sel_act_dt = 'df_mtcars')
+
+    expect_true(session$userData$dt$act_name == 'df_mtcars')
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 mtcars |> as.data.table()
+    )
+
+    # delete dataset
+    session$userData$dt$dt[['df_cars']] <- NULL
+
+    expect_true(session$userData$dt$act_name == 'df_mtcars')
+    expect_equal(session$userData$dt$dt |> names(), c('df_iris', 'df_mtcars'))
+    expect_equal(session$userData$dt$dt[[session$userData$dt$act_name]],
+                 mtcars |> as.data.table())
+  })
+})
+
 # test df info UI -------------------------------------------------------------
-test_that("Test render df_info UI", {
+test_that('Test render df_info UI', {
   testServer(sidebar_server, args = list(app_session = NULL), {
 
     session$userData$dt <- reactiveValues(
@@ -51,13 +148,13 @@ test_that("Test render df_info UI", {
     expect_true(grepl('5.0 / 2\n', output$df_info$html))
 
     expect_true(grepl("Columns with NA's:\n", output$df_info$html))
-    expect_true(grepl("1", output$df_info$html))
+    expect_true(grepl('1', output$df_info$html))
 
   })
 })
 
 # test render preview ---------------------------------------------------------
-test_that("Test renders dataset preview first dataset", {
+test_that('Test renders dataset preview first dataset', {
   testServer(sidebar_server, args = list(app_session = NULL), {
 
     session$userData$dt <- reactiveValues(
@@ -84,7 +181,7 @@ test_that("Test renders dataset preview first dataset", {
   })
 })
 
-test_that("Test renders dataset preview second dataset", {
+test_that('Test renders dataset preview second dataset', {
   testServer(sidebar_server, args = list(app_session = NULL), {
 
     session$userData$dt <- reactiveValues(
